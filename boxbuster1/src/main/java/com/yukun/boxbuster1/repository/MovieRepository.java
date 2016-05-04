521a03735ba216152dc82fecd957316dcd3d3bd7
AKUN <AKUN@172.20.206.225>
2016-05-03 17:43:37 -0700

    Updated MovieRepository and MovieRepositoryImpl

--- boxbuster1/src/main/java/com/yukun/boxbuster1/repository/MovieRepository.java
+++ boxbuster1/src/main/java/com/yukun/boxbuster1/repository/MovieRepository.java
@@ -1,5 +1,19 @@
 package com.yukun.boxbuster1.repository;
 
-public interface MovieRepository {
+import java.util.List;
 
+import com.yukun.boxbuster1.entity.Movie;
+
+public interface MovieRepository {
+	long addMovie(Movie movie);
+
+	Movie getMovieByTitle(String name);
+
+	Movie getMovieByRate(float iMDBRating);
+
+	Movie getMovieByGenre(String genre);
+
+	List<Movie> getAllMovies();
+
+	void update(Movie movie);
 }
--- boxbuster1/src/main/java/com/yukun/boxbuster1/repository/impl/MovieRepositoryImpl.java
+++ boxbuster1/src/main/java/com/yukun/boxbuster1/repository/impl/MovieRepositoryImpl.java
@@ -1,5 +1,13 @@
 package com.yukun.boxbuster1.repository.impl;
 
-public class MovieRepositoryImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.yukun.boxbuster1.repository.MovieRepository;
+
+@Repository
+public class MovieRepositoryImpl implements MovieRepository {
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 }
